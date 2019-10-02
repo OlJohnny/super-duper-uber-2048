282df818c588462ea5a6569c5e75df72ee18b87a
OlJohnny <Jonatwebhan@gmail.com>
2019-10-02 16:38:01 +0800

    initial build of game
    doesn't work to the same rules as the real 20148, *yet*

---
+++ src/auto_2048/game.java
@@ -0,0 +1,244 @@
+package auto_2048;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class game {
+	public static int matrix_zeilen = 4;
+	public static int matrix_spalten = 4;
+	
+	public static int[][] main_matrix = new int[matrix_zeilen][matrix_spalten];
+	
+	public static void main(String[] args) {
+		long timer_start = System.currentTimeMillis();
+		
+		// GAME ABLAUF
+		int game_over = 0;
+		while (game_over == 0) {
+			if (add_random() == 1) {
+				System.out.println("Score: " + get_matrix_score() + "\n");
+				print_matrix();
+				System.out.println("-----------------\n");
+				// lese User Input
+				int open_scanner = 1;
+				while (open_scanner == 1) {
+					char input = 'o';
+					System.out.print("Please enter your next move (w, a, s, d): ");
+					Scanner in = new Scanner(System.in);
+					input = in.next().charAt(0);
+					if (input == 'w') {
+						open_scanner = 0;
+						master_matrix_shifter("to_top");
+					} else if (input == 'a') {
+						open_scanner = 0;
+						master_matrix_shifter("to_left");
+					} else if (input == 's') {
+						open_scanner = 0;
+						master_matrix_shifter("to_bottom");
+					} else if (input == 'd') {
+						open_scanner = 0;
+						master_matrix_shifter("to_right");
+					}
+				}
+			} else {
+				game_over = 1;
+				System.out.println("Score: " + get_matrix_score() + "\n");
+				print_matrix();
+				System.out.println("\nGAME OVER");
+			}
+		}
+		
+		System.out.println("\n=================\nRUNTIME: " + ((System.currentTimeMillis() - timer_start) / 1000.0) + " s");
+	}
+	
+	
+	public static int add_random() {
+		if (matrix_full() == 1) {
+			// Wenn jeder Slot der Matrix voll -> Game Over
+			return 0;
+		} else {
+			while (true) {
+				int freie_breite = get_random(matrix_zeilen);
+				int freie_spalte = get_random(matrix_spalten);
+				
+				if (main_matrix[freie_breite][freie_spalte] == 0) {
+					// 2 oder 4 in leeres random feld schreiben? 3:1 wahrscheinlichkeit
+					if (get_random(3) < 2) {
+						main_matrix[freie_breite][freie_spalte] = 2;
+					} else {
+						main_matrix[freie_breite][freie_spalte] = 4;
+					}
+					// erfolgreich random zahl eingefügt
+					return 1;
+				}
+			}
+		}
+	}
+	
+	
+	public static void print_matrix() {
+		// find largest number
+		int max_length = 0;
+		for (int i=0; i<matrix_zeilen; i++) {
+			for (int j=0; j<matrix_spalten; j++) {
+				if (max_length < Integer.toString(main_matrix[i][j]).length()) {
+					max_length = Integer.toString(main_matrix[i][j]).length();
+				}
+			}
+		}
+		// pretty print
+		for (int i=0; i<matrix_zeilen; i++) {
+			System.out.print("|");
+			for (int j=0; j<matrix_spalten; j++) {
+				String current = Integer.toString(main_matrix[i][j]);
+				// replace "0" with "." for more readability
+				if (current.equals("0")) {
+					current = ".";
+				}
+				// equallize length for largest appearing number
+				while (current.length() < (max_length + 2)) {	// '+2' for spacer between columns
+					current = " " + current;
+				}
+				// print in orderly fashion
+				if (j == matrix_spalten-1) {
+					System.out.println(current + " |");
+				} else {
+					System.out.print(current);
+				}
+			}
+		}
+	}
+	
+	
+	public static int get_random(int range) {
+		// return random int in range 0...'range'-1
+		return (int)(Math.random() * range);
+	}
+	
+	
+	public static int matrix_full() {
+		int full_space_counter = 0;
+		// zähle wie viele Felder der Matrix einen Wert haben
+		for (int i=0; i<matrix_zeilen; i++) {
+			for (int j=0; j<matrix_spalten; j++) {
+				if (main_matrix[i][j] != 0) {
+					full_space_counter++;
+				}
+			}
+		}
+		if (full_space_counter == (matrix_zeilen * matrix_spalten)) {
+			// Matrix ist voll
+			return 1;
+		} else {
+			// Matrix ist nicht voll
+			return 0;
+		}
+	}
+	
+	
+	public static int get_matrix_score() {
+		int score = 0;
+		for (int i=0; i<matrix_zeilen; i++) {
+			for (int j=0; j<matrix_spalten; j++) {
+				score += main_matrix[i][j];
+			}
+		}
+		return score;
+	}
+	
+	
+	public static void master_matrix_shifter(String direction) {
+		int[][] current_matrix = new int[matrix_zeilen][matrix_spalten];
+		int[][] last_matrix = new int[matrix_zeilen][matrix_spalten];
+		// 1. copy main_matrix into current_matrix
+		for (int i=0; i<matrix_zeilen; i++) {
+			for (int j=0; j<matrix_spalten; j++) {
+				current_matrix[i][j] = main_matrix[i][j];
+			}
+		}
+		int iterator = 0;
+		while (!Arrays.deepEquals(current_matrix, last_matrix)/*current_matrix.equals(last_matrix)*/) {
+			// 2. copy current_matrix into last_matrix
+			for (int i=0; i<matrix_zeilen; i++) {
+				for (int j=0; j<matrix_spalten; j++) {
+					last_matrix[i][j] = current_matrix[i][j];
+				}
+			}
+			// 3. shift current_matrix
+			// call slave_matrix_shifter(current_matrix[][], "to_right")
+			current_matrix = slave_matrix_shifter(current_matrix, direction);
+			
+		}
+		// 4. if current_matrix is equal to last_matrix, copy current_matrix into main_matrix
+		for (int i=0; i<matrix_zeilen; i++) {
+			for (int j=0; j<matrix_spalten; j++) {
+				main_matrix[i][j] = current_matrix[i][j];
+			}
+		}
+	}
+	
+	
+	public static int[][] slave_matrix_shifter(int[][] current_matrix, String direction) {
+		if (direction.equals("to_left")) {
+			// laufe spalten von der zweit-ersten spalte aus durch bis zur letzten spalte
+			for (int j=1; j<matrix_spalten; j++) {
+				// laufe zeilen von oben aus komplett durch
+				for (int i=0; i<matrix_zeilen; i++) {
+					if (current_matrix[i][j-1] == 0) {
+						current_matrix[i][j-1] = current_matrix[i][j];
+						current_matrix[i][j] = 0;
+					} else if (current_matrix[i][j-1] == current_matrix[i][j]) {
+						current_matrix[i][j-1] = current_matrix[i][j-1] + current_matrix[i][j];
+						current_matrix[i][j] = 0;
+					}
+				}
+			}
+		} else if (direction.equals("to_right")) {
+			// laufe spalten von der zweit-letzten spalte aus durch bis zur ersten spalte
+			for (int j=matrix_spalten-2; j>-1; j--) {
+				// laufe zeilen von oben aus komplett durch
+				for (int i=0; i<matrix_zeilen; i++) {
+					if (current_matrix[i][j+1] == 0) {
+						current_matrix[i][j+1] = current_matrix[i][j];
+						current_matrix[i][j] = 0;
+					} else if (current_matrix[i][j+1] == current_matrix[i][j]) {
+						current_matrix[i][j+1] = current_matrix[i][j+1] + current_matrix[i][j];
+						current_matrix[i][j] = 0;
+					}
+				}
+			}
+		} else if (direction.equals("to_top")) {
+			// laufe zeilen von der zweit-obersten bis zur untersten zeile
+			for (int i=1; i<matrix_zeilen; i++) {
+				// laufe spalten von links nach rechts komplett durch
+				for (int j=0; j<matrix_spalten; j++) {
+					if (current_matrix[i-1][j] == 0) {
+						current_matrix[i-1][j] = current_matrix[i][j];
+						current_matrix[i][j] = 0;
+					} else if (current_matrix[i-1][j] == current_matrix[i][j]) {
+						current_matrix[i-1][j] = current_matrix[i-1][j] + current_matrix[i][j];
+						current_matrix[i][j] = 0;
+					}
+				}
+			}
+			
+		} else if (direction.equals("to_bottom")) {
+			// laufe zeilen von der zweit-untersten bis zur obersten zeile
+			for (int i=matrix_zeilen-2; i>-1; i--) {
+				// laufe spalten von links nach rechts komplett durch
+				for (int j=0; j<matrix_spalten; j++) {
+					if (current_matrix[i+1][j] == 0) {
+						current_matrix[i+1][j] = current_matrix[i][j];
+						current_matrix[i][j] = 0;
+					} else if (current_matrix[i+1][j] == current_matrix[i][j]) {
+						current_matrix[i+1][j] = current_matrix[i+1][j] + current_matrix[i][j];
+						current_matrix[i][j] = 0;
+					}
+				}
+			}
+		}
+		// return shifted matrix
+		return current_matrix;
+	}
+
+}
